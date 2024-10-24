1111aba08b21c4cc272cfd5e4ae3088f17b3dd33
jacobvigliotti <63595188+jacobvigliotti@users.noreply.github.com>
2024-10-24 13:44:54 -0500

    Added relationships via foreign keys

--- groupSixProjectSchema.sql
+++ groupSixProjectSchema.sql
@@ -14,7 +14,9 @@ create table games (
     gameType int,				/*Int for game type. 1 could represent casual, 2 for competetive. Boolean might work better here, but then id change the attribute name to isCompetetive*/
     gameDate date,
     gameLoc varchar(20),
-    primary key(gameID)
+    gameHost int,
+    primary key(gameID),
+    foreign key (gameHost) references users(userID)
     );
 	
 	
@@ -29,18 +31,19 @@ create table transactions (
 	transID int, 				/*int to represent id of this transaction */
 	cardInstID int,				/* int to represent the id of the card instance*/
     initiator int,				/* int to represent the userID of the seller*/
-    initiatedOn date, 			/*Not sure if Initiated On meant the date, or somthing else*/
+    initiatedOn date, 			
     price float,
     buyer int,					/*int for the UserID of the buyer*/
+    primary key (transID, cardInstID, initiator),
 	foreign key (initiator) references users(userID),
-    foreign key (cardInstID) references cardInstances(cardInstID),
-    primary key (transID, cardInstID, initiator)
+    foreign key (cardInstID) references cardInstances(cardInstID)
 	);
 create table decks (
 	deckID int,
     deckName varchar(20),
     deckOwner int,
-    primary key(deckID)
+    primary key(deckID),
+    foreign key (deckOwner) references users(userID)
     );
     
 create table cards (
@@ -49,7 +52,8 @@ create table cards (
     cardColor varchar(20),		
     cardType varchar(20),
     cardCreator int,
-    primary key(cardNum)
+    primary key(cardNum),
+    foreign key (cardCreator) references users(userID)
     );
     
 create table artworks (
@@ -57,15 +61,18 @@ create table artworks (
     artFranchise varchar(20),
     artCharacter varchar(20),
     illustrator int,
-    primary key(artID)
+    primary key(artID),
+    foreign key (illustrator) references users(userID)
     );
     
 create table cardInstances (
 	cardInstID int,
 	cardNum int, 
     cardOwner int,
-    cardCond int,				/*int to represent condition on a scale of 1 to 5*/
-    foreign key (cardNum) references cards(cardNum),
+    cardCond int, /*int to represent condition on a scale of 1 to 5*/
+    cardDeck int, 
+    primary key(cardInstId, cardNum),
+	foreign key (cardNum) references cards(cardNum),
     foreign key (cardOwner) references users(userID),
-    primary key(cardInstId, cardNum)
+    foreign key (cardDeck) references decks(deckID)
     );
